005cae859a85073fe20949155b5aad758dcae1f3
Rowan Strang <rowanstrang@gmail.com>
2025-05-06 10:12:26 +1200

    inital commit

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world, this is rowan, testing commits")
         }
         .padding()
     }
