63cab7aa5857a60eec658da656b04e0a1ee338ba
Rowan Strang <rowanstrang@gmail.com>
2025-05-06 10:17:31 +1200

    removed stack, added text

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,13 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world, this is rowan, testing commits")
-        }
-        .padding()
+        Text("This is rowan")
+        Text("This is another text view")
     }
 }
 
