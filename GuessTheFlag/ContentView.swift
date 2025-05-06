d518ea44ae6254d5d828c26d8cf49d7e28c31f32
Rowan Strang <rowanstrang@gmail.com>
2025-05-07 08:37:08 +1200

    tapping the flag gives the player an answer

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,18 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    var countries = ["estonia", "france", "germany", "ireland", "italy", "nigeria", "poland", "spain", "uk", "us"].shuffled()
+    var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var selectedChoices = ""
     
     var body: some View {
-        Button("Show Alert"){
-            showingAlert = true
+        ZStack{
+            LinearGradient(
+                gradient: Gradient(colors: [.blue.opacity(0.6), .blue]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            VStack(spacing: 30){
+                VStack{
+                    Text(scoreTitle)
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer].capitalized)
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button{
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                    }
+                }
+            }
         }
-        .alert("Rowanies Button", isPresented: $showingAlert) {
-            Button("Please God No", role: .cancel) {}
-            Button("Please God No", role: .destructive) {}
-        } message: {
-            Text("Please Read this")
+    }
+    
+    func flagTapped(_ number: Int){
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
         }
+        showingScore = true
     }
 }
 
